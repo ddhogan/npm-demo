80043663b55357aa3204a182ae994833805863af
Donna <donnamcla@gmail.com>
2018-10-21 14:39:29 -0400

    add route handler for put requests

--- express-demo/index.js
+++ express-demo/index.js
@@ -20,11 +20,11 @@ app.get('/api/courses', (req, res) => {
   res.send(courses);
 })
 
+// Create
 app.post('/api/courses', (req, res) => {
   const schema = { //this is the shape of the course object
     name: Joi.string().min(3).required(),
   };
-
   const result = Joi.validate(req.body, schema);
   
   // input validation
@@ -33,7 +33,6 @@ app.post('/api/courses', (req, res) => {
     res.status(400).send(result.error.details[0].message);
     return;
   }
-
   const course = {
     id: courses.length + 1,
     name: req.body.name,
@@ -42,10 +41,33 @@ app.post('/api/courses', (req, res) => {
   res.send(course); // by convention, return the thing you just made
 })
 
+// Retrieve
 app.get('/api/courses/:id', (req, res) => {
   const course = courses.find(c => c.id === parseInt(req.params.id));
   if (!course) res.status(404).send('The course with the given ID was not found');
   res.send(course); 
+})
+
+// Update
+app.put('/api/courses/:id', (req, res) => {
+  // find the course
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send('The course with the given ID was not found');
+   
+  // validate the input
+  const schema = { //this is the shape of the course object
+    name: Joi.string().min(3).required(),
+  };
+  const result = Joi.validate(req.body, schema);
+  if (result.error) {
+    // RESTful convention is to return a response with HTTP status code 400: bad request
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+  // update the course
+  course.name = req.body.name;
+  // return the course
+  res.send(course);
 })
 
 // in a hosted environment, port gets assigned dynamically, so we need to use:
@@ -54,5 +76,5 @@ app.get('/api/courses/:id', (req, res) => {
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`))
 
-// in terminal: `export PORT=5000` to export the evironment variable PORT and set it equal to 5000
+// in terminal: `export PORT=5000` to export the environment variable PORT and set it equal to 5000
 // in terminal in windows, use 'set'
